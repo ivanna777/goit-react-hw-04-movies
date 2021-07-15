78c75a3b6dbe8d4cb311e6688bd34d0046f6227d
Ivanna Kirij <ivannakirij@gmail.com>
2021-07-15 23:01:32 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
+import { Suspense, lazy } from 'react';
 import { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from "./views/HomePage";
-import MoviesPage from "./views/MoviesPage";
-import MovieDetailsPage from "./views/MovieDetailsPage";
 import AppBar from './components/AppBar';
 import routes from './route';
  
-// const HomePage = lazy(() => import('./views/HomePage.js' /* webpackChunkName: "HomePage" */));
-// const MoviesPage = lazy(() => import('./views/MoviesPage.js' /* webpackChunkName: "MoviesPage" */));
-// const MovieDetailsPage = lazy(() => import('./views/MovieDetailsPage.js' /* webpackChunkName: "MovieDetailsPage" */));
+const HomePage = lazy(() => import('./views/HomePage.js' /* webpackChunkName: "HomePage" */));
+const MoviesPage = lazy(() => import('./views/MoviesPage.js' /* webpackChunkName: "MoviesPage" */));
+const MovieDetailsPage = lazy(() => import('./views/MovieDetailsPage.js' /* webpackChunkName: "MovieDetailsPage" */));
 
 class App extends Component{
   render() {
     return (
       <>
     <AppBar />
-    {/* <Suspense fullback={<h1>Loading...</h1>}> */}
+    <Suspense fallback={<h1>Loading...</h1>}>
         <Switch>
           <Route exact path={routes.home} component={HomePage} />
           <Route exact path={routes.movies} component={MoviesPage} />
           <Route path={routes.movieDetail} component={MovieDetailsPage} />
           <Route component={HomePage} />
         </Switch>
-      {/* </Suspense> */}
+      </Suspense>
   </>
     )
   }
